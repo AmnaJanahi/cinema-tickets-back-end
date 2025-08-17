8b8b1a82ebba044e7984b9e1ad73dbbc44ee11fb
Amna Janahi <amna.m.janahi@gmail.com>
2025-08-17 11:08:28 +0300

    adding showBooking in the routes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,6 +4,7 @@ const bookingController = require('../controllers/bookingController')
 
 router.post('/add-booking', bookingController.createBooking);
 router.get('/', bookingController.bookingIndex);
+router.get('/:id',bookingController.showBooking )
 
 
 module.exports = router
