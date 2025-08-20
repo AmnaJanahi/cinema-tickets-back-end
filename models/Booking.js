9c9af5e3886bf15afa4a0cd0a8d9c4f62870d153
Bayan <bayanessam40@gmail.com>
2025-08-20 12:29:01 +0300

    updating

--- models/Booking.js
+++ models/Booking.js
@@ -1,38 +1,32 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
-const bookingSchema = new Schema(
-  {
-    // movieId: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "Movie",
-    //   required: true,
-    // },
-    name: {
-      type: String,
-      required: true,
-    }
-  //   date: {
-  //     type: Date,
-  //     required: true,
-  //   },
-  //   timing: {
-  //     type: String,
-  //     enum: ["13:00", "15:30", "17:45", "19:30", "23:00", "00:15"],
-  //     required: true,
-  //   },
-  //   seat: [{
-  //     type: String,
-  //     required: true
-  //   }],
-  //   userId: {
-  //     type: mongoose.Schema.Types.ObjectId,
-  //     ref: "User",
-  //     required: true,
-  //   },
+const bookingSchema = new Schema({
+  movieId: {
+    type: Schema.Types.ObjectId,
+    ref: "Movie",
+    required: true,
   },
-  // { timestamps: true }
-);
+
+  date: {
+    type: Date,
+    required: true,
+  },
+  timing: {
+    type: String,
+    enum: ["13:00", "15:30", "17:45", "19:30", "23:00", "00:15"],
+    required: false,
+  },
+  seat: [{
+    type: String,
+    required: true
+  }],
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    // required: true, 
+  },
+}, { timestamps: true });
 
 const Booking = model("Booking", bookingSchema);
-
 module.exports = Booking;
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const bookingController = require('../controllers/bookingController')
 
+
 router.post('/add-booking', bookingController.createBooking);
 router.get('/', bookingController.bookingIndex);
 router.get('/:id',bookingController.showBooking )
